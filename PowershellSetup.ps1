a8a7bbca233ed6426269647d1ca73e5cc5212a21
Knox Lively <knox.lively@protonmail.com>
2024-05-17 12:55:24 -0500

    refactored heavily...test

--- PowershellSetup.ps1
+++ PowershellSetup.ps1
@@ -1,35 +1,52 @@
 ### minimal setup script to load PowerShell configs, vimrc, and other misc tasks
 
-# Pre-reqs:
+### Pre-reqs:
 # 1. Copy ssh folder (github key + configs) to local users SSH dir
 
 # Clone windows repo
-Set-Location $env:USERPROFILE
-if (!(Test-Path -Path "Repos")) {
-    New-Item -ItemType Directory -Path "Repos"
+$repoPath=(Join-Path $env:USERPROFILE "Repos")
+$windowsRepoPath=(Join-Path $repoPath "windows")
+
+if (Test-Path -Path $repoPath) {
+	if (Test-Path -Path $windowsRepoPath)
+       Set-Location $windowsRepoPath
+	   git pull
+	else
+       Set-Location $repoPath
+       git clone git@github.com:gonzolively/windows.git
+
+if (!(Test-Path -Path $repoPath)) {
+    New-Item -ItemType Directory -Path $repoPath
+    git clone git@github.com:gonzolively/windows.git $repoPath
 }
-Set-Location "Repos"
-git clone git@github.com:gonzolively/windows.git
 
 # Symlink PowerShell Folder
 $powershellFolder = Join-Path $env:USERPROFILE "Documents\WindowsPowerShell"
 if (Test-Path -Path $powershellFolder) {
     Remove-Item -Path $powershellFolder -Force -Recurse -Confirm:$false
 }
-New-Item -ItemType SymbolicLink -Path $powershellFolder -Value (Join-Path $env:USERPROFILE "Repos\windows\WindowsPowerShell")
+New-Item -ItemType SymbolicLink -Path $powershellFolder -Value (Join-Path $windowsRepoPath "\WindowsPowerShell")
 
 # Symlink PowerShell profile to ISE profile
 $iseProfilePath = Join-Path $powershellFolder "Microsoft.PowerShellISE_profile.ps1"
-$iseProfileTarget = Join-Path $env:USERPROFILE "Repos\windows\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$iseProfileTarget = Join-Path $windowsRepoPath "\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 if (Test-Path -Path $iseProfilePath) {
     Remove-Item -Path $iseProfilePath -Force -Confirm:$false
 }
 New-Item -ItemType SymbolicLink -Path $iseProfilePath -Value $iseProfileTarget -Force
 
+# Symlink Terminal Settings
+$terminalSettingsPath = Join-Path $env:USERPROFILE "AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+if (Test-Path -Path $terminalSettingsPath) {
+    Remove-Item -Path $terminalSettingsPath -Force -Recurse -Confirm:$false
+}
+$terminalSettingsTarget = Join-Path $windowsRepoPath "\Terminal Settings\LocalState"
+New-Item -ItemType SymbolicLink -Path $terminalSettingsPath -Value $terminalSettingsTarget
+
 ### Vim Stuff
-# Copy symlink vimrc
+# Set up vimrc file
 $vimrcPath = "C:\tools\vim\_vimrc"
-$vimrcTarget = Join-Path $env:USERPROFILE "Repos\windows\_vimrc"
+$vimrcTarget = Join-Path $repoPath "\windows\_vimrc"
 if (Test-Path -Path $vimrcPath) {
     Remove-Item -Path $vimrcPath -Force -Confirm:$false
 }
@@ -50,15 +67,6 @@ if (-Not (Test-Path $vundlePath)) {
     Write-Host "Vundle is already installed at $vundlePath."
 }
 
-# Install vim plugins
-$vimrcPath = "C:\tools\vim\_vimrc"
+# Install vim plugins with bundle
 Write-Host "Installing Vim Plugins..."
 vim -E -s -u "$vimrcPath" -c "BundleInstall" -c "qa!"
-
-# Terminal Settings
-$terminalSettingsPath = Join-Path $env:USERPROFILE "AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-if (Test-Path -Path $terminalSettingsPath) {
-    Remove-Item -Path $terminalSettingsPath -Force -Recurse -Confirm:$false
-}
-$terminalSettingsTarget = Join-Path $env:USERPROFILE "Repos\windows\Terminal Settings\LocalState"
-New-Item -ItemType SymbolicLink -Path $terminalSettingsPath -Value $terminalSettingsTarget
